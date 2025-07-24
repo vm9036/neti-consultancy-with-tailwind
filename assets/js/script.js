b7bcce349cf527f1d42fd683f2fffba6623b72fd
vm9036 <107696677+vm9036@users.noreply.github.com>
2025-07-24 22:06:21 +0530

    Update neti consultancy index page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+// animation on scroll
+// AOS.init({
+//     once: true,
+//     duration: 1000,
+// });
+
+// function toggleMenu() {
+//     const menu = document.getElementById("mobileMenu");
+//     menu.classList.toggle("hidden");
+// }
